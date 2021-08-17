5b2569385780be9254c8952319753378c5377717
Elijah Winterton <elijah@wintertons.us>
2021-08-17 02:02:35 -0400

    changes to add NFT feed clean up create react app stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import React from "react";
+
+const options = { method: "GET" };
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const divStyle = { 
+    display: "flex", 
+    flexDirection: "column",
+    margin: '15px'
+  }
+
+  const getData = () => {
+    fetch(
+      "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setData(response))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    if (data.length === 0) {
+      getData();
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>NFT Art of Talented Artists</p>
       </header>
+      <main className="App-main" >
+        {data.assets &&
+          data.assets.map((c) => {
+            return (
+              <div id={c.token_id} style={divStyle}>
+                <img src={c.collection.featured_image_url} />
+                <span>{c.collection.name}</span>
+              </div>
+            );
+          })}
+        </main>
+      
     </div>
   );
 }
