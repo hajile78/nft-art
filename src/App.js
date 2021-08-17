cf4dfb7def6686d747a5379e72be006692793a32
Elijah Winterton <elijah@wintertons.us>
2021-08-17 15:34:35 -0400

    fixed the fields and added link to buy image

--- src/App.js
+++ src/App.js
@@ -15,11 +15,12 @@ function App() {
     justifyContent: "space-around",
   };
 
+  // const url = "https://api.opensea.io/api/v1/assets?owner=0x00056a746ccc5bc2fb05b4c1e6e274b8d1816739&order_direction=desc&offset=0&limit=20"
+  const url =
+    "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20";
+
   const getData = () => {
-    fetch(
-      "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20",
-      options
-    )
+    fetch(url, options)
       .then((response) => response.json())
       .then((response) => setData(response))
       .catch((err) => console.error(err));
@@ -40,8 +41,10 @@ function App() {
           data.assets.map((c) => {
             return (
               <div id={c.token_id} style={divStyle}>
-                <img src={c.collection.featured_image_url} height="100%" alt="" />
-                <span>{c.collection.name}</span>
+                <a href={c.permalink}>
+                  <img src={c.image_preview_url} height="100%" alt="" />
+                </a>
+                <span>{c.name}</span>
               </div>
             );
           })}
